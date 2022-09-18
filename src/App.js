2f01680453e31151c2c0e33231d2961d5a18e87e
Richard <richard.swmr@gmail.com>
2022-09-19 02:16:30 +1200

    Added a first draft of content to the home page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { Container, Row, Col } from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-    </div>
+    // Landing Page
+    <Container>
+      <Row>
+        <Col>
+        <h1>Coming soon..</h1></Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Kia Ora! My name is Richard, I'm a Communication and Web Designer based in Takapuna, in beautiful Tāmaki Makaurau, New Zealand.</p>
+          <p>Look&See is my design portfolio showcasing projects I've found rewarding while also revealing the 'behind-the-scenes'.</p>
+          <p>I want it to be a deep dive into my process as a designer, and all the tangents and explorations that come before the finished outcomes.</p>
+          <p>I believe in design driven by detail; by care, craft and collaboration.</p>
+          <p>Graduating from Auckland University of Technology with a Bachelor of Communication Design in 2018, I recently returned to study and am completing a Diploma of Web and User Experience Design at Yoobee Colleges, and a Diploma of Web Development at Whitecliffe.</p>
+        </Col>
+      </Row>
+      <Col>
+      <p>+64226472489</p>
+      <p>hello@lookandsee.design</p>
+      </Col>
+      <Col>
+        instagram
+        behance
+      </Col>
+      <Row>
+      </Row>
+    </Container>
   );
 }
 

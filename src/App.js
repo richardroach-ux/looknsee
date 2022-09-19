dd4489bef592981d01355c2e548d88aa7be562ee
Richard <richard.swmr@gmail.com>
2022-09-19 15:57:15 +1200

    Minor spelling corrections.

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
           <p>Kia Ora!</p>
           <p>My name is Richard, I'm a Communication and Web Designer based in Takapuna, in beautiful Tāmaki Makaurau, New Zealand.
           <br></br><br></br>Look&See is my design portfolio showcasing projects I've found rewarding, and the work behind the work.
-          <br></br>A deep dive into my process as a designer and all the tangents and explorations that come before the finished outcomes.</p>
+          <br></br>It's a deep dive into my process as a designer and all the tangents and explorations that come before the finished outcomes.</p>
           <p>I believe in design driven by detail; by care, craft and collaboration.</p>
           {/* <p>Graduating from Auckland University of Technology with a Bachelor of Communication Design in 2018, I recently returned to study and am completing a Diploma of Web and User Experience Design at Yoobee Colleges, and a Diploma of Web Development at Whitecliffe.</p> */}
-          <p>I am available for freelance work while I look for a at a creative agency. <br></br>#openforwork</p>
+          <p>I am available for freelance work while I look for a role at a creative agency. <br></br>#openforwork</p>
           <br></br>
         </Col>
       </Row>

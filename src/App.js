42502a23e592f36d9d6395eb4bd27690620dbe43
Richard <richard.swmr@gmail.com>
2022-09-19 13:42:15 +1200

    Adding further content.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <Container>
       <Row>
         <Col>
-        <h1>Coming soon..</h1></Col>
+        <h2>Coming soon..</h2></Col>
       </Row>
       <Row>
         <Col>
@@ -13,7 +13,8 @@ function App() {
           <p>Look&See is my design portfolio showcasing projects I've found rewarding while also revealing the 'behind-the-scenes'.</p>
           <p>I want it to be a deep dive into my process as a designer, and all the tangents and explorations that come before the finished outcomes.</p>
           <p>I believe in design driven by detail; by care, craft and collaboration.</p>
-          <p>Graduating from Auckland University of Technology with a Bachelor of Communication Design in 2018, I recently returned to study and am completing a Diploma of Web and User Experience Design at Yoobee Colleges, and a Diploma of Web Development at Whitecliffe.</p>
+          {/* <p>Graduating from Auckland University of Technology with a Bachelor of Communication Design in 2018, I recently returned to study and am completing a Diploma of Web and User Experience Design at Yoobee Colleges, and a Diploma of Web Development at Whitecliffe.</p> */}
+          <p>I am currently #openforwork, and available for freelance work while I look for a creative role at a creative agency.</p>
         </Col>
       </Row>
       <Col>

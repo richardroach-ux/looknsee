3df59da475350f087af59cf0df34b62b38222e37
Richard <richard.swmr@gmail.com>
2022-09-24 20:37:16 +1200

    Added breakpoints and display area.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 function App() {
   return (
     // Landing Page
     <Container fluid>
-      <Row className='header-spacer'/>
-      <Row>
-        <Col xs={{offset: 1}}>
+      <Row className='h-100 no-gutters' xs={1} md={2}>
+        <Col className='column-one' xl={5}>
         <h1>Coming soon... 👀</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} sm={{span: 8, offset: 2}}>
-        <br></br>
           <p>Kia Ora!</p>
-          <p>My name is Richard, I'm a Communication and Web Designer based in Takapuna, in beautiful Tāmaki Makaurau, New Zealand.
+          <p>My name is Richard, I'm a Communication and Web Designer based in beautiful Tāmaki Makaurau, New Zealand.
           <br></br><br></br>Look&See is my design portfolio showcasing projects I've found rewarding, and the work behind the work.
           <br></br>It's a deep dive into my process as a designer and all the tangents and explorations that come before the finished outcomes.</p>
           <p>I believe in design driven by detail; by care, craft and collaboration.</p>
-          {/* <p>Graduating from Auckland University of Technology with a Bachelor of Communication Design in 2018, I recently returned to study and am completing a Diploma of Web and User Experience Design at Yoobee Colleges, and a Diploma of Web Development at Whitecliffe.</p> */}
-          <p>I am available for freelance work while I look for a role at a creative agency. <br></br>#openforwork</p>
-          <br></br>
+          {/* <p>After graduating from AUT with a Bachelor of Communication Design in 2018 I returned to study both at Whitecliffe and Yoobee, this time focusing on digital design.</p>
+          <p>As a result I am comfortable across both print and digital and my work reflects the value this range of study has given me.</p> */}
+          <p>I'm available for freelance work while I look for a role at a creative agency. Wink wink.<br></br><br></br>#openforwork</p>
+          <h2>Project in mind? Get in touch! 📣</h2>
+                  <p>+64226472489</p>
+                  <p>hello@lookandsee.design</p>
+                  <br></br>
+                <div className='contact-container'>
+                  <a><p>resume</p></a>
+                  <a><p>instagram</p></a>
+                  <a><p>behance</p></a>
+                </div>
         </Col>
-      </Row>
-      <Col xs={{offset: 1}}>
-      <h1>Get in touch! 📣</h1>
-      </Col>
-      <Col xs={12} sm={{span: 8, offset: 2}}>
-        <br></br>
-        <p>+64226472489</p>
-        <p>hello@lookandsee.design</p>
-        <br></br>
-        <a><p>resume</p></a>
-        <a><p>instagram</p></a>
-        <a><p>behance</p></a>
-      </Col>
-      <Row>
+        <Col xl={7}>
+          <div className='gallery-container'>
+            <div id='redBox'>
+              Display
+            </div>
+          </div>
+          
+        </Col>
       </Row>
     </Container>
   );
